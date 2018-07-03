7e74b52c6e3fe0731203239706e18fb55c525121
snavdeepsingh <snavdeepsingh@gmail.com>
2018-07-03 14:29:42 -0500

    added api route to load avaolable users

--- client/src/components/pages/Invite.js
+++ client/src/components/pages/Invite.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React,{Component} from "react";
 import firebase from "firebase";
 import FriendsList from "../FriendsList";
 import { Paper, Typography } from "@material-ui/core";
 import "./PageBody.css";
+import API from "../../utils/API";
 
 
 const styles = {
@@ -10,26 +11,53 @@ const styles = {
   FriendsList: { padding: 20, width: 600 }
 }
 
-const Invite = () =>
-  <div className='page-body'>
-    <Paper style={styles.Paper}>
-      {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
-      <Typography variant='Title'>
-      <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
-        Welcome {firebase.auth().currentUser.displayName}! You are signed in.
-      </Typography> 
-      <br />
 
-        <Typography variant='display1'>Invite</Typography>
-    </Paper>
-    <br />
+class Invite extends Component{
 
+  state = {
+    users: [],
+    user: firebase.auth().currentUser,
 
-   <FriendsList />
+  }
 
-  </div>;
+  componentDidMount = () => {
+    this.getUsersCurrentlyOnline()
+
+  }
 
 
+
+  getUsersCurrentlyOnline = () => {
+    API.getUsers({"isSignedIn":true})
+    .then(res => this.setState({users:res.data, user:firebase.auth().currentUser}))
+    .then(console.log("These are the users on line",this.state.users))
+    .catch(err => console.log(err));
+  }
+
+
+  render(){
+    return(
+      <div className='page-body'>
+      <Paper style={styles.Paper}>
+        {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
+        <Typography variant='Title'>
+        <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
+          Welcome {firebase.auth().currentUser.displayName}! You are signed in.
+        </Typography> 
+        <br />
   
+          <Typography variant='display1'>Invite</Typography>
+      </Paper>
+      <br />
+  
+  
+     <FriendsList />
+  
+    </div>
+  
+    )
+  }
+
+}
 
 export default Invite;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,7 @@ module.exports = {
     findAll: function(req, res) {
       db
         .find(req.query)
+        .sort({userName:1})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
