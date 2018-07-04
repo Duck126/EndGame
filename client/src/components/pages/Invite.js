fd807241a475ecb5135eae0f8afb8306275170e9
Pierce Morrill <piercemorrill91@gmail.com>
2018-07-03 19:39:33 -0500

    placesAPI starting

--- client/src/components/pages/Invite.js
+++ client/src/components/pages/Invite.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import firebase from "firebase";
 import FriendsList from "../FriendsList";
-import { Paper, Typography } from "@material-ui/core";
+import Inputs from "../Inputs";
+import TimePicker from "../TimePicker";
+import { Paper, Typography, Grid } from "@material-ui/core";
 import "./PageBody.css";
-
+import API from "../../utils/API";
 
 const styles = {
-  Paper: { padding: 20, width: 600 },
-  FriendsList: { padding: 20, width: 600 }
+  Paper: { padding: 20, width: 600, margin: 'auto' },
 }
 
-const Invite = () =>
-  <div className='page-body'>
-    <Paper style={styles.Paper}>
-      {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
-      <Typography variant='Title'>
-      <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
-        Welcome {firebase.auth().currentUser.displayName}! You are signed in.
-      </Typography> 
-      <br />
+  class Invite extends Component { 
+    constructor(props) {
+      super(props); 
+      this.state = {
+        isSignedIn: true,
+        user: {},
+        value: 0,
+        checked: false,
+        liveUsers: [], 
+      };
+    };
+  
+    handleChange = name => event => {
+      this.setState({ [name]: event.target.checked = true });
+    };
+  
+    componentDidMount() {
+      API.getUsers()
+      .then((response)=> {
+        this.setState(()=>{
+          return {
+            liveUsers: response.data
+          }
+        })
+      })
+      .catch(err=>console.log(err))
+    }    
+    
+    
+    render(){
+      let users = this.state.liveUsers;
+      return (
+          <div className='page-body'>
+            <Grid container spacing={24}> 
+            <Grid item xs={12}> 
+                <Paper style={styles.Paper}>
+                  {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
+                    <Typography variant='Title'>
+                    <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
+                    Welcome {firebase.auth().currentUser.displayName}! You are signed in.
+                    </Typography> 
+                <br />
 
               <Typography variant='display1'>Invite</Typography>
               </Paper>
@@ -34,12 +68,11 @@ const Invite = () =>
               handleChange = {this.state.handleChange}/>
             </Grid>
 
+            </Grid>
+          </div>
+        )
+      }
+  }
 
-   <FriendsList />
-
-  </div>;
-
-
-  
 
 export default Invite;
